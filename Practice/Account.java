63dcd73d8c9dafe2e255bc054abce1bf3990829b
Catalin Alexandru <33178694+CatalinOAlexandru@users.noreply.github.com>
2018-01-11 17:22:00 +0000

    Practice W1

--- Practice/Account.java
+++ Practice/Account.java
@@ -1,27 +1,48 @@
 public class Account
 {
-//instance variables
-private double balance;
-
-//constructors
- public Account(double initialBalance) {
- balance = initialBalance;
- }
-
- public Account() {
- balance = 0.0;
- } 
- //instance methods
- public void withdraw(double amount) {
- balance -=amount;
- }
- public void deposit(double amount) {
- balance +=amount;
- }
- public double getBalance() {
- return balance;
- }
- public void close() {
- balance = 0.0;
- }
+	//instance variables
+	private double balance;
+	//constructors
+	public Account(double initialBalance) 
+	{
+		balance = initialBalance;
+		if(balance < 0)
+		{
+			System.out.println("Sorry, but you can't open an Account with a negative balance");
+			balance = 0.0;
+			System.out.println("Your current balance is 0.0");
+		}
+	}
+	public Account() 
+	{
+		balance = 0.0;
+	} 
+	//instance methods
+	public void withdraw(double amount) 
+	{
+		balance -=amount;
+		if(balance < 0)
+		{
+			balance += amount;
+			System.out.println("Your current balance is " +balance+ ".");
+			System.out.println("Sorry, but you can't withdraw "+amount+" and go under 0.");
+			
+		}
+	}
+	public void deposit(double amount) 
+	{
+		balance +=amount;
+		if(balance > 100000)
+		{
+			System.out.println("You are now rich!");
+		}
+	}
+	public double getBalance() 
+	{
+		return balance;
+	}
+	public void close() 
+	{
+		balance = 0.0;
+	}
 }
--- Practice/TestAccount.java
+++ Practice/TestAccount.java
@@ -1,6 +1,6 @@
 public class TestAccount {
  public static void main(String[] args) {
- Account account1 = new Account(23000);
+ Account account1 = new Account();
  System.out.println("Balance in account1 is " + account1.getBalance());
  }
  }
